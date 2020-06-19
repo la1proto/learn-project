6765f5ac3c51c6edb7c23909d05da1054a5e0e49
Zorka <zorka@LA-MacBook.local>
2020-06-20 01:01:56 +0300

    new srv

--- srv.py
+++ srv.py
@@ -23,63 +23,49 @@ def message_bye(hour):
 
 def page_goodbye(qs):
     return f"""
-        {bye(datetime.today().hour)}
+        {message_bye(datetime.today().hour)}
         time: {datetime.today()}
             """
 
-def get_n(qs):
-    if qs == "":
-        return "anonymouse"
+def get_name(qs):
+    if "name" in qs:
+    	return qs["name"][0]
     else:
-        qs = parse_qs(qs)
-        if "name" not in qs:
-            return "anonymouse"
-        else:
-            return qs["name"][0]
+    	return "KTO Tbl?"
 
-def get_y(qs):
-    if qs == "":
-        return "X3"
+def get_year(qs):
+    if "age" in qs:
+    	today = datetime.today().year
+    	age = int(qs["age"][0])
+    	return str(today - age)
     else:
-        qs = parse_qs(qs)
-        if "age" not in qs:
-            return "X3"
-        else:
-            today = datetime.today().year
-            return str(today - int(qs["age"][0]))
-
-def page(query):
-    path, qs = query.split("?") if '?' in query else [query,""]
-    path = path.rstrip('/')
-
-    switcher = {
-        "/hello": page_hello,
-        "/goodbye": page_goodbye,
-    }
-
-    return switcher[path](qs) if path in switcher else "X3 page"
+    	return "X3"
 
 def page_hello(qs):
-    qs = parse_qs(qs) if qs != "" else ""
-    name = get_n(qs)
-    year = get_y(qs)
+    if qs != "":
+    	qs = parse_qs(qs)
+    name = get_name(qs)
+    year = get_year(qs)
     return f"""
-                    Hi {name}! 
-                    You were born in {year}.
-                """
+    hi {name}
+    you were born in {year}
+    """
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
-            if self.path != "":
-                msg = page(self.path)
+        path, qs = self.path.split("?") if '?' in self.path else [self.path, ""]
+        path = path.rstrip('/')
+        switcher = {"/hello": page_hello, "/goodbye": page_goodbye}
+        if path in switcher:
+            msg = switcher[path](qs)
 
-                self.send_response(200)
-                self.send_header("Content-type", "text/plain")
-                self.send_header("Content-length", str(len(msg)))
-                self.end_headers()
-                self.wfile.write(msg.encode())
-            else:
-                return SimpleHTTPRequestHandler.do_GET(self)
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.send_header("Content-length", str(len(msg)))
+            self.end_headers()
+            self.wfile.write(msg.encode())
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self)
         
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
